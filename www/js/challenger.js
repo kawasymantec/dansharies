482c8628c1206ee58268d6617c781cfe069d99d1
sumioka <sumioka.motoshi@gmail.com>
2016-10-13 08:38:25 +0900

    APIキーのところを自分のにしてコミットしちゃいました。修正です。

--- www/js/challenger.js
+++ www/js/challenger.js
@@ -8,7 +8,8 @@ myApp.factory('challenger',function($http){
     var username = "";
     var password = "";
     // ここのキー情報を書き換えてください
-    data.ncmb = new NCMB("4b88bd93e5559645c2bb49bf28d61955600bcba834647b2921be0e0b6d35349c","bc4389b1d9c193ce52772f59d6331f699a364a860826d4178e45ba3cb4334bb1");
+    data.ncmb = new NCMB("38b0271001ed27f11c37c96c48a22c3450b80531d8fe34691b840fa9fa78b276","aca37c2ba53f426e752addb2310361771343832ab94360c3dfae65b874d0932a");
+//    data.ncmb = new NCMB("4b88bd93e5559645c2bb49bf28d61955600bcba834647b2921be0e0b6d35349c","bc4389b1d9c193ce52772f59d6331f699a364a860826d4178e45ba3cb4334bb1");
 
     //
     data.currentMission = null;
@@ -138,7 +139,8 @@ myApp.factory('challenger',function($http){
             });
     };
     
-    data.baasurlbase = "https://mb.api.cloud.nifty.com/2013-09-01/applications/lML0I3RGoykDdhm/publicFiles/";
+    data.baasurlbase = "https://mb.api.cloud.nifty.com/2013-09-01/applications/J2MyTSYNctp6tx7e/publicFiles/";
+//    data.baasurlbase = "https://mb.api.cloud.nifty.com/2013-09-01/applications/lML0I3RGoykDdhm/publicFiles/";
     /*
         ログイン中かどうかを返す
         true: ログイン中　false: 未ログイン
