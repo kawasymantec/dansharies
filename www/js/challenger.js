7262804f4fcb3fc3512091efb1a5b3eb27aa423a
takamee <n.takamichi@jp.fujitsu.com>
2016-10-17 15:51:33 +0900

    成績表示画面を作成（西島によるデバッグ用箇所削除）

--- www/js/challenger.js
+++ www/js/challenger.js
@@ -8,10 +8,10 @@ myApp.factory('challenger',function($http){
     var username = "";
     var password = "";
     // ここのキー情報を書き換えてください
-//    data.ncmb = new NCMB("38b0271001ed27f11c37c96c48a22c3450b80531d8fe34691b840fa9fa78b276","aca37c2ba53f426e752addb2310361771343832ab94360c3dfae65b874d0932a");
+    data.ncmb = new NCMB("38b0271001ed27f11c37c96c48a22c3450b80531d8fe34691b840fa9fa78b276","aca37c2ba53f426e752addb2310361771343832ab94360c3dfae65b874d0932a");
 //    data.ncmb = new NCMB("4b88bd93e5559645c2bb49bf28d61955600bcba834647b2921be0e0b6d35349c","bc4389b1d9c193ce52772f59d6331f699a364a860826d4178e45ba3cb4334bb1");
     //アプリケーションキー・クライアントキー
-  data.ncmb = new NCMB("1c86df0315639d025352c9605c008b6923da7b4825c04595cbefde5425826f81","4861b858964d8842849b1bcd94c0442be08c0b34250c4b79cc96115e3a57319a");
+//  data.ncmb = new NCMB("1c86df0315639d025352c9605c008b6923da7b4825c04595cbefde5425826f81","4861b858964d8842849b1bcd94c0442be08c0b34250c4b79cc96115e3a57319a");
 
     //
     data.currentMission = null;
@@ -141,9 +141,9 @@ myApp.factory('challenger',function($http){
             });
     };
     
-//    data.baasurlbase = "https://mb.api.cloud.nifty.com/2013-09-01/applications/J2MyTSYNctp6tx7e/publicFiles/";
+    data.baasurlbase = "https://mb.api.cloud.nifty.com/2013-09-01/applications/J2MyTSYNctp6tx7e/publicFiles/";
 //    data.baasurlbase = "https://mb.api.cloud.nifty.com/2013-09-01/applications/lML0I3RGoykDdhm/publicFiles/";
-    data.baasurlbase = "https://mb.api.cloud.nifty.com/2013-09-01/applications/HRXHqc5QIzTcC1zL/publicFiles/";
+//    data.baasurlbase = "https://mb.api.cloud.nifty.com/2013-09-01/applications/HRXHqc5QIzTcC1zL/publicFiles/";
 
 /*
         ログイン中かどうかを返す
